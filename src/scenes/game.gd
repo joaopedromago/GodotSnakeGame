ff1c4f94abc0ef86c2d06d39cae75287e006a01d
João Mago <zaconou@gmail.com>
2024-01-09 20:09:03 -0300

    feat: implementing game scene menu

--- src/scenes/game.gd
+++ src/scenes/game.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const NavigationService = preload("res://src/services/navigation_service.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_return_pressed():
+	var navigation_service = NavigationService.new();
+	navigation_service.navigate_to_menu(self)
